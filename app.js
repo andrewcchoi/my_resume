a32fe4a5f6787b522d78fcab890140e0726bd183
Andrew Choi <74335726+andrewcchoi@users.noreply.github.com>
2021-10-26 19:56:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const body = document.querySelector("body");
 const animateProgressBar = () =>{
     let scrollOffset = document.documentElement.clientHeight - 80
     let scrollDistance = -body.getBoundingClientRect().top + scrollOffset;
+    let scrollHeight = body.getBoundingClientRect().height;
     let progressWidth = Math.floor((scrollDistance / scrollHeight) * 100);
 
     progressBar.style.width = progressWidth + "%";
     if (progressWidth <= 23) {
         progressBar.style.width = "0%";
-    }
-    
-    console.log(scrollDistance, scrollHeight, progressWidth);
+    }   
 }
 
 window.addEventListener("scroll", animateProgressBar);
